0a650462e9e5a9ed39b41f91a91db9750b1c8990
Christopher Heinz <heinz@cs.uni-kl.de>
2018-09-11 10:26:53 +0200

    bumped to OpenHAB Version 2.4.0 Fixes, so Maven will build properly in commandline

--- src/main/java/org/openhab/binding/vicinityadapter/internal/vcntobject/VICINITYobject.java
+++ src/main/java/org/openhab/binding/vicinityadapter/internal/vcntobject/VICINITYobject.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.vicinityadapter.internal.vcntobject;
 
 import java.util.ArrayList;
@@ -10,6 +18,13 @@ import org.slf4j.LoggerFactory;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+
+/**
+ * This class acts as a REST resource to access and control all devices exposed to VICINITY and is registered with the
+ * Jersey servlet.
+ *
+ * @author Christopher Heinz - Initial contribution
+ */
 
 public class VICINITYobject {
 
